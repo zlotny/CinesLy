a58b8e51ca8298036bf9cec071d6f176910587b9
Javier Villalobos Santamarina <lazajavier@gmail.com>
2014-12-09 20:21:50 +0100

    Modicada cabecera

--- catalogo.php
+++ catalogo.php
@@ -8,8 +8,8 @@
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
-    <link href="bootstrap/js/bootstrap.js" rel="stylesheet" />
-
+    <link href="bootstrap/js/bootstrap.js" rel="text/javascript" />
+    <link href="style/style.css" rel="stylesheet" />
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
