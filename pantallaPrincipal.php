94809faf1a09323ba1bd615906499595ed32c52b
Gabriel Dominguez <jdgomez@esei.uvigo.es>
2014-12-10 19:14:43 +0100

    pantallaPincipal v0.2

--- pantallaPrincipal.php
+++ pantallaPrincipal.php
@@ -15,14 +15,28 @@
 </head>
 <body>
 	<?php cabeceraPantallaPrincipal();?>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="col-md-3"><img src="img/logo.png" alt="logo" class="responsive" height="100%" width="100%"></div>
+				<div class="col-md-9">
+					<input type="text" class="form-control" placeholder="Escribe un comentario....">
+				</div>
 
+
+
+
+			</div>
+			
+		</div>
+
+	</div>
 	
 	<script src="javascript/jquery-2.1.1.min.js"></script>
 	<script src="bootstrap/js/bootstrap.min.js"></script>
 	<script src="javascript/docs.min.js"></script>
 	<script src="javascript/ie10-viewport-bug-workaround.js"></script>
-
-
 	<script type="text/javascript" src="javascript/jssor.slider.min.js"></script>
 </body>
 </html>
