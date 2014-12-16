4593bf09f70d322fceb6cc5efbe2b0c39d6f27f5
Isaac González <igdx1989@gmail.com>
2014-12-16 21:13:37 +0100

    editados perfil.php, style.css

--- perfil.php
+++ perfil.php
@@ -1,183 +1,129 @@
-<?php include_once "cabecera.php";
-  include_once "modelos/usuario.php";
- ?>
-
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php
+	include "cabecera.php" ?>
 
-  <title>Perfil de Usuario</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">	
+	<link rel="stylesheet" href="style/style.css">
 
-  <!-- Bootstrap core CSS -->
-  <link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
-  <link href="style/style.css" rel="stylesheet" />
+</head>
+<body>
+	<?php
+	cabeceraPantallaPrincipal();
+	?>
+	<div class="top-margin">
+		<div class="col-md-3" >
+				<div class="col-md-1" ></div>
+				<div class="col-md-10 divPerfil">
+					<img src="img/default_user.png" width="150px" class="img-rounded">
+				<div><p><br><strong>Nombre</p></div>	
+				<div><p><br><strong>Correo</p><br></div>
+				<input type="button" class "btn btn-info" data-toggle="modal" data-target="#del" value="Editar Cuenta">
+				</div>
+				<div class="col-md-1" ></div>
+			
+		</div>
 
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <![endif]-->
-    </head>
-    <body>
-      <?php cabeceraPantallaPrincipal(); ?>
-
-<?php 
-session_start();
-
- if(!(isset($_SESSION['usuario']->email))) {
-      echo "error";
-    }?>
-      <div class="col-md-2" ></div>
-      <!-- 1º COLUMNA -->
-      <div class="col-md-4">
-        <div class="container top-margin">  
-          <div class="col-md-2 ">
-            <img src="img/default_user.png" width="150px" class="img-rounded">
-          </div>
-
-          <div class="col-md-2" id="test">
-            <table>
-              <tr><td><label for="Nombre Usuario" class="col-lg control-label"><?php echo $_SESSION['usuario']->nombreUsuario; ?></label></td></tr>
-              <tr><td><label for="Correo" class="col-lg control-label"><?php echo $_SESSION['usuario']->email; ?></label></td></tr>
-            </table>
-          </div>
-        </div>
-
-        <form class="form-horizontal top-margin" role="form">
-
-          <div class="form-group" >
-          <label for="contraseña" class="col-lg-3 ">Contraseña</label>
-            <div class="col-lg-5">
-              <input type="password" class="form-control" id="contraseña" placeholder="Contraseña">
-            </div>
-          </div>
-
-          <div class="form-group">
-            <label for="nueva_contraseña" class="col-lg-3 ">Nueva Contraseña</label>
-            <div class="col-lg-5">
-              <input type="password" class="form-control" id="nueva_contraseña" placeholder="Nueva Contraseña">
-            </div>
-          </div>
-
-          <div class="form-group">
-            <label for="repita_contraseña" class="col-lg-3 ">Repetir Contraseña</label>
-            <div class="col-lg-5">
-              <input type="password" class="form-control" id="repita_contraseña" placeholder="Repetir contraseña">
-            </div>
-          </div>
-
-          <div class="form-group">
-            <label for="cambiar_correo" class="col-lg-3 control-label">Cambiar Correo</label>
-            <div class="col-lg-5">
-              <input type="text" class="form-control" id="cambiar_correo" placeholder="Cambiar Correo">
-            </div>
-          </div>
-
-        </form>
-
-        <form>
-          <div class="col-md-8  col-md-offset-1">
-            <table class="table table-bordered">
-
-              <tr>
-                <th colspan="2">Películas recomendadas</th>
-              </tr>
-
-              <tr>
-                <td><label>Pelicula 1</label></td>
-                <td><input type="checkbox" value="" checked>Eliminar</td>
-              </tr>
-
-              <tr>
-                <td><label>Pelicula 2</label></td>
-                <td><input type="checkbox" value="" checked>Eliminar</td>
-              </tr>
-
-              <tr>
-                <td><label>Pelicula 3</label></td>
-                <td><input type="checkbox" value="" checked>Eliminar</td>
-              </tr>
-
-              <tr>
-                <td><label>Pelicula 4</label></td>
-                <td><input type="checkbox" value="" checked>Eliminar</td>
-              </tr>
-
-            </table>
-          </div>
-        </form>
-      </div>
-    </div>
-    <!-- 2º COLUMNA -->
-    <div class="col-md-4" id="test">
-      <table class="table table-bordered">
-
-        <tr>
-          <th colspan="2">Publicaciones</th>
-        </tr>
-
-        <tr>
-          <td><label>Publicacion 1</label></td>
-          <td><input type="checkbox" value="" checked>Eliminar</td>
-        </tr>
-
-        <tr>
-          <td><label>Publicacion 2</label></td>
-          <td><input type="checkbox" value="" checked>Eliminar</td>
-        </tr>
-
-        <tr>
-          <td><label>Publicacion 3</label></td>
-          <td><input type="checkbox" value="" checked>Eliminar</td>
-        </tr>
-
-        <tr>
-          <td><label>Publicacion 4</label></td>
-          <td><input type="checkbox" value="" checked>Eliminar</td>
-        </tr>
-      </table>
-
-      <input type="button" class="btn btn-info" data-toggle="modal" data-target="#del" value="Eliminar cuenta">
-      <div id="del" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-        <form action=<?php echo "controladoras/procesarUsuario.php?email=".$_SESSION['usuario']->email ?> method="POST">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h3>Eliminar cuenta</h3>
-              </div>
-              <div class="modal-body"> 
-                <input type="text" name="pass" class="form-control form-pers" placeholder="Introduzca su contraseña">          
-              </div>
-              <div class="modal-footer">
-                <input type="submit" name="accion" class="btn btn-success" value="eliminar">
-                <button type="button" class="btn btn-prmary" data-dismiss="modal">Cerrar</button>
-              </div>
-            </div>
-          </div>
-        </form>
-      </div> 
-      <a href="amigos.html" class="btn btn-info">Validar</a>
-
-
-    </div>
-
-    <div class="col-md-2" ></div>
-
-
-  </body>
+		<div class="col-md-5" >
+			<div class="panel panel-default">
+				<div class="panel-heading">Publicaciones</div>
+				<div class="panel-body scrollable-table-perfilC">
+					<table class="table table-striped">
+						<tr class="table-publicaciones"><td><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></td>
+						</tr>
+						<tr class="table-publicaciones"><td><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></td>
+						</tr>
+						<tr class="table-publicaciones"><td><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></td>
+						</tr>
+						<tr class="table-publicaciones"><td><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></td>
+						</tr>
+						<tr class="table-publicaciones"><td><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></td>
+						</tr>
+						<tr class="table-publicaciones"><td><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+							Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
 
 
 
-
-  <script src="javascript/jquery-2.1.1.min.js"></script>
-  <script src="bootstrap/js/bootstrap.js"></script>
+		<div class="col-md-3" >
 
 
+			<div class="panel panel-default">
+				<div class="panel-heading">Últimas Películas Vistas</div>
+				<div class="panel-body scrollable-table-perfilD">
+					<table class="table table-striped ">
+						<tr><td>Película Vista 1</td></tr>
+						<tr><td>Película Vista 2</td></tr>
+						<tr><td>Película Vista 3</td></tr>
+						<tr><td>Película Vista 4</td></tr>
+						<tr><td>Película Vista 5</td></tr>
+						<tr><td>Película Vista 6</td></tr>
+						<tr><td>Película Vista 7</td></tr>
+						<tr><td>Película Vista 8</td></tr>
+						<tr><td>Película Vista 9</td></tr>
+						<tr><td>Película Vista 10</td></tr>
+						<tr><td>Película Vista 11</td></tr>
+						<tr><td>Película Vista 12</td></tr>
 
 
+					</table>
+				</div>
+			</div>
+
+			<div class="panel panel-default">
+				<div class="panel-heading">Películas Recomendadas</div>
+				<div class="panel-body scrollable-table-perfilD">
+					<table class="table table-striped ">
+						<tr><td>Película Recomendada 1</td></tr>
+						<tr><td>Película Recomendada 2</td></tr>
+						<tr><td>Película Recomendada 3</td></tr>
+						<tr><td>Película Recomendada 4</td></tr>
+						<tr><td>Película Recomendada 5</td></tr>
+						<tr><td>Película Recomendada 6</td></tr>
+						<tr><td>Película Recomendada 7</td></tr>
+						<tr><td>Película Recomendada 8</td></tr>
+						<tr><td>Película Recomendada 9</td></tr>
+						<tr><td>Película Recomendada 10</td></tr>
+						<tr><td>Película Recomendada 11</td></tr>
+						<tr><td>Película Recomendada 12</td></tr>
 
 
+					</table>
+				</div>
+			</div>
+		</div>
+
+		<div class="col-md-1"></div>
+
+
+	</div>
+</body>
+
+<script src= "javascript/jquery-2.1.1.min.js"></script>
+<script src= "bootstrap/js/bootstrap.js"></script>
+
+</html>
