74546bb0974983be9e3a9d4a80827ac2970b1ec9
Manuel Lorenzo Tallon <mlorenzotallon@gmail.com>
2014-12-16 12:15:27 +0100

    Modificada plantilla sesion.php

--- modelos/sesion.php
+++ modelos/sesion.php
@@ -2,31 +2,27 @@
 
 class Sesion{
 
-	var $titulo;
-	var $director;
 	var $sala;
 	var $fechaSesion;
 	var $capacidad;
+	var $idSesion;
+	var $idPelicula;
 
 
 
-	function __construct($titulo,$director,$sala,$fechaSesion,$capacidad){
+	function __construct($idSesion,$sala,$fechaSesion,$capacidad,$idPelicula){
 		
-		$this->titulo=$titulo;
-		$this->director=$director;
+		$this->idSesion=$idSesion;
 		$this->sala=$sala;
 		$this->fechaSesion=$fechaSesion;
 	 	$this->capacidad=$capacidad;
+	 	$this->idPelicula=$idPelicula;
 
 	}
 
 	/*GETTERS*/
-	function getTitulo(){
-		return $titulo;
-	}
-
-	function getDirector(){
-		return $director;
+	function getIdSesion(){
+		return $idSesion;
 	}
 
 	function getSala(){
@@ -37,10 +33,15 @@ class Sesion{
 		return $fechaSesion;
 	}
 
+	function getIdPelicula(){
+		return $idPelicula;
+	}
+
 	function getCapacidad(){
 		return $capacidad;
 	}
 
+	/*
 	function getObjetoSesion($titulo,$director){
 		$this->conectarBD();
 		$sql_1="SELECT * FROM sesion WHERE titulo= '".$titulo."' AND director ='".$director."'";
@@ -56,24 +57,25 @@ class Sesion{
 			return $sesion;
 		}
 	}
-
+	*/
 
 	/*SETTERS*/
 
-	function setTitulo($titulo){
-		$this->titulo=$titulo;
+	function setIdSesion($idSesion){
+		$this->idSesion=$idSesion;
 	}
 
-	function setDirector($director){
-		$this->director=$director;
-	}
-
+	
 	function setSala($sala){
 		$this->sala=$sala;
 	}
 
 	function setFechaSesion($fechaSesion){
 		$this->fechaSesion=$fechaSesion;
+	}
+
+	function setHoraSesion($horaSesion){
+		$this->horaSesion=$horaSesion;
 	}
 
 	function setCapacidad($capacidad){
@@ -96,6 +98,39 @@ class Sesion{
 		}
 		return $resultado;
 
+	}
+
+	function anhadirSesion($idPelicula,$Sesion){
+
+
+	}
+
+	function eliminarSesion($idPelicula,$idSesion){
+
+		$this->conectarBD();
+		$sql="DELETE FROM sesion WHERE idSesion ='".$idSesion."' and idPelicula = '".$idPelicula."'";
+		$this->consultaBD($sql);
+		header("location:sesion.php");
+	}
+
+	function modificarSesion($idPelicula,$idSesion){
+
+		
+	}
+
+	function consultarSesion($idSesion,$idPelicula){
+
+		$this->conectarBD();
+		$sql="SELECT * FROM sesion WHERE idSesion ='".$idSesion."' and idPelicula = '".$idPelicula."'";
+		$resultado=mysql_query($sql);
+		$row=mysql_num_rows($resultado);
+		if(row==1){
+			$row=mysql_fetch_array($resultado);
+			return new Sesion($sesi["idPelicula"],$sesi["idSesion"],$sesi["sala"],$sesi["fechaSesion"],$sesi["capacidad"]);
+		}else{
+			echo "La sesion no existe.";
+		}
+		
 	}
 
 
