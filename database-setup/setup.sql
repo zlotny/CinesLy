552ae928da8ef64f56fd78cb99cfce58a2f5a06d
Javier Villalobos Santamarina <lazajavier@gmail.com>
2014-12-22 19:29:36 +0100

    modificada BD todas las id son autoincrementales

--- database-setup/setup.sql
+++ database-setup/setup.sql
@@ -10,20 +10,23 @@ drop table if exists pelicula;
 drop table if exists agrega;
 drop table if exists usuario;
 
-
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+CREATE DATABASE `CinesLy` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci;
+USE `CinesLy`;
 
 create table usuario(
     nombreUsuario	varchar(20)	not null,
     email		varchar(50)	primary key,
-    pass		varchar(10)	not null,
+    pass		varchar(20)	not null,
     foto			varchar(50),
     preferencia1	varchar(20),
     preferencia2	varchar(20),
     preferencia3	varchar(20),
     estado		varchar(20),
-    ciudad_actual	varchar(20),
-    fecha_nacimiento	date,
-    tipol	int	not null,
+    ciudadActual	varchar(20),
+    fechaNacimiento	date,
+    tipoUsuario	int	not null,
     eslogan		varchar(140)
 );
 
@@ -37,69 +40,69 @@ create table agrega(
     foreign key(email1) references usuario(email) on delete cascade on update cascade,
     foreign key(email2) references usuario(email) on delete cascade on update cascade
 );
-
+
 
 create table pelicula(
-    titulo			varchar(50),
-    director		varchar(50),
-    distribuidora		varchar(50),
-    duracion		varchar(10), /*time*/
-    sinopsis		varchar(500),
-    actores		varchar(200),
-    año			year,
-    fecha_estreno	date,
-    genero		varchar(20),
-    pais			varchar(20),
-    votos		int,
-    valoracion		int,
-    tipo			varchar(20) not null,
-    cont_valoracion	int,
+	idPelicula int not null auto_increment,
+	titulo        	varchar(50),
+	director    	varchar(50),
+	distribuidora    	varchar(50),
+	duracion    	varchar(10),
+	sinopsis    	varchar(500),
+	actores    	varchar(200),
+	anho        	year,
+	fecha_estreno	date,
+	genero    	varchar(50),
+	pais        	varchar(20),
+	votos    	int,
+	valoracion    	int,
+	tipo        	enum('cartelera','especial','proximamente') not null,
+	cont_valoracion	int,
+	foto                     	varchar(500),
 
-
-primary key(titulo,director)
+	primary key(idPelicula)
 );
+
 
 create table comenta(
-    titulo       		varchar(50),
-    director       		varchar(50),
+  	idPelicula   int,
     email		varchar(50),
     comentario       	varchar(140), 
+    fecha		datetime,
     
-    primary key(titulo,director,email),
-    foreign key(titulo,director) references pelicula(titulo,director) on delete cascade on update cascade,
-     foreign key(email) references usuario(email) on delete cascade on update cascade
+    primary key(idPelicula,email,comentario),
+    foreign key(idPelicula) references pelicula(idPelicula) on delete cascade on update cascade,
+    foreign key(email) references usuario(email) on delete cascade on update cascade
 );
 
-
 
 create table sesion(
-    titulo			varchar(50),
-    director		varchar(50),
-    fecha		datetime,
-    sala			int,
-    capacidad		int,
+	idPelicula         	int not null,
+	idSesion           	int not null auto_increment,
+	fecha    	datetime,
+	sala        	int,
+	capacidad    	int,
 
-    primary key(titulo,director,fecha,sala),
-    foreign key(titulo,director) references pelicula(titulo,director) on delete cascade on update cascade
-    );
+	primary key(idSesion),
+	foreign key(idPelicula) references pelicula(idPelicula) on delete cascade on update cascade
+	);
 
 create table evento(
-    id_evento		varchar(10),    
-    titulo			varchar(50),
-    director		varchar(50),
-    fecha_sesion	datetime,
-    sala			int,
-    email		varchar(50),
-    descripcion		varchar(140),
-    nombre 		varchar(20),	
-    primary key(id_evento),
-    foreign key(titulo,director,fecha_sesion,sala) references  sesion(titulo,director,fecha,sala) on delete cascade on update cascade,
-    foreign key(email) references usuario(email) on delete cascade on update cascade
+	id_evento    	int not null auto_increment,    
+	idSesion      	int not null,
+	email    	varchar(50) not null,
+	descripcion    	varchar(140),
+	nombre     	varchar(20) not null,   
+ 
+	primary key(id_evento),
+	foreign key(idSesion) references  sesion (idSesion) on delete cascade on update cascade,
+	foreign key(email) references usuario(email) on delete cascade on update cascade
    );
 
 
+
   create table contiene(
-  id_evento	varchar(10),
+  id_evento	int,
   email		varchar(50),
   primary key(id_evento, email),
   foreign key(id_evento) references evento(id_evento) on delete cascade on update cascade,
