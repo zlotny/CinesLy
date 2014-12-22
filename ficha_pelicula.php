0e143b6aca87c12eeda5c85948938f0021bd225c
Andrés Vieira <anvieiravazquez@gmail.com>
2014-12-22 02:52:58 +0100

    Slightly changed movie comments desing. Now you can access any user profile by clicking on his photo

--- ficha_pelicula.php
+++ ficha_pelicula.php
@@ -50,7 +50,7 @@ include_once "sesion_segura.php";
     </div>
     <div class="col-lg-9">
      <div class="panel panel-default top-margin">
-     <div class="panel-heading text-weight-bold">Ficha de la Película</div>
+       <div class="panel-heading text-weight-bold">Ficha de la Película</div>
        <div class="panel-body">
         <div class="row-fluid">
           <div class="span6">
@@ -65,59 +65,70 @@ include_once "sesion_segura.php";
               </div>
             </div>
           </div>
-        </div>
-        <div class="input-group col-md-6 " style="margin:0px 0px 3px 0px">
-          <span class="input-group-btn">
 
-            <form method="POST" class="pull-right" action="controladoras/inserComentPelControlador.php" >
-              <input type="hidden" value="<?php echo $ObjPeli->idPelicula; ?>" name="idPeli"/>
-              <input type="text" class="form-control" placeholder="Escribe un comentario" name="coments"/>
-              <input type="submit" class="btn btn-info" value="Publicar"/>					
-            </form>
-          </span>
-        </div>
-        <div class="top-margin">
-          <div class="share-box-outer">
-            <h4>Comentarios película</h4>
-          </div>
-          <ul class="media-list">
-            <?php 					
-
-            $arrayComentarios=Pelicula::getComentariosPelicula($ObjPeli->idPelicula);
-            $arrayUsuarios=$arrayComentarios['usuario'];
-            $arrayComents=$arrayComentarios['comentario'];
-
-            for($i=0; $i<sizeof($arrayComents); $i++){
-             $usuActual = Usuario::getObjetoUsuario($arrayUsuarios[$i]);
-             $cmtActual = $arrayComents[$i];
-
-             ?>
-
-             <li class="media">
-
-              <div class="well">
-
-                <a class="media-left" href="#">
-                  <img src="img/default_user.png" alt="" height="50px" width="50px" class="thumbnail">
-                </a>
-
-                <div class="media-body">
-                  <h4 class="media-heading"><?php echo $usuActual->nombreUsuario; ?></h4>
-                  <p><?php echo $cmtActual; ?></p>
+          
+          <div class="top-margin">
+            <div class="share-box-outer">
+              <div class="panel panel-default">
+                <div class=" text-weight-bold panel-heading">Comentarios</div>
+                <div class="panel-body" >
+                 <div class="input-group col-md-11" style="margin-left: -10px;" >
+                   <span class="input-group-btn ">
+                    <form method="POST" action="controladoras/inserComentPelControlador.php" >
+                      <input type="hidden" value="<?php echo $ObjPeli->idPelicula; ?>" name="idPeli"/>
+                      <input type="text" class="form-control" placeholder="Escribe un comentario" name="coments"/>
+                      <input type="submit" class="btn btn-info" value="Publicar"/>          
+                    </form>
+                  </span>
                 </div>
               </div>
-            </li>
+              <ul class="media-list">
+                <?php 					
 
-            <?php
-          }
-          ?>
-        </ul>
+                $arrayComentarios=Pelicula::getComentariosPelicula($ObjPeli->idPelicula);
+                $arrayUsuarios=$arrayComentarios['usuario'];
+                $arrayComents=$arrayComentarios['comentario'];
+
+                for($i=0; $i<sizeof($arrayComents); $i++){
+                 $usuActual = Usuario::getObjetoUsuario($arrayUsuarios[$i]);
+                 $cmtActual = $arrayComents[$i];
+
+                 ?>
+
+                 <li class="media">
+                  <div class="well">
+                    <a class="media-left" href="perfilAmigo.php?email=<?php echo $usuActual->email; ?>">
+                      <?php
+
+                      if(isset($usuActual->foto)){
+                        echo "<img src='$usuActual->foto' alt='' height='50px' width='50px' class='thumbnail'>";
+
+                      }else{
+                        echo "<img src='img/default_user.png' alt='' height='50px' width='50px' class='thumbnail'>";
+
+                      }
+
+                      ?>
+                    </a>
+                    <div class="media-body">
+                      <h4 class="media-heading"><?php echo $usuActual->nombreUsuario; ?></h4>
+                      <p><?php echo $cmtActual; ?></p>
+                    </div>
+                  </div>
+                </li>
+
+                <?php
+              }
+              ?>
+            </div>
+          </ul>
+        </div>
       </div>
-
-
-
     </div>
+
+
   </div>
+</div>
 </div>
 </div>
 <script src="js/jquery-2.1.1.min.js"></script>
