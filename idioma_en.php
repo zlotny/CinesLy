f9583e888ae2bf5e16cad3906110318f11bf0d4c
Lorena Docasar <docasarlorena@gmail.com>
2014-12-25 23:01:58 +0100

    Creado idioma_jap.php

--- idioma_en.php
+++ idioma_en.php
@@ -2,9 +2,10 @@
 
 //aquí se van a definir variables globales
 //define(name, value)
-define("SALUDOS", "Welcome fucking gays");
-define("CAMBIARIDIOMA","here you can switch the languaje" );
+define("SALUDOS", "Welcome");
+define("CAMBIARIDIOMA","here you can switch the language" );
 define("CAMBIOIDIOMAEN", "switch to english");
 define("CAMBIOIDIOMAES", "cambiar a espanhol");
+define("CAMBIOIDIOMAJAP", "日本人に切り替え");
 
 ?>
